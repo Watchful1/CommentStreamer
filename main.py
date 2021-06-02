cd648590c908e1c3402751e83bad5fd51ef45410
Watchful1 <watchful@watchful.gr>
2021-06-02 15:07:38 -0700

    Bit of cleanup

--- main.py
+++ main.py
@@ -58,10 +58,10 @@ async def worker(name, queue, reddit):
 			response_object = await reddit.get(endpoints.API_PATH["info"], params={"id": api_request.id_string()})
 			reddit_objects = response_object.children
 			if len(reddit_objects) == 0:
-				log.info(f"{name} fetched {api_request.ids[0][3:]} to {api_request.ids[-1][3:]}, got {len(reddit_objects)} objects, requeueing")
+				log.debug(f"{name} fetched {api_request.ids[0][3:]} to {api_request.ids[-1][3:]}, got {len(reddit_objects)} objects, requeueing")
 				queue.put_nowait(api_request)
 			else:
-				log.info(f"{name} fetched {api_request.ids[0][3:]} to {api_request.ids[-1][3:]}, got {len(reddit_objects)} objects")
+				log.debug(f"{name} fetched {api_request.ids[0][3:]} to {api_request.ids[-1][3:]}, got {len(reddit_objects)} objects")
 				api_request.results = reddit_objects
 
 		except Exception as e:
@@ -75,12 +75,17 @@ async def producer(queue, start_id):
 	latest_datetime = None
 	while True:
 		now = datetime.utcnow()
-		log.info(f"producer: {queue.qsize()} : {len(request_list)}")
+		log.debug(f"producer: {queue.qsize()} : {len(request_list)}")
 
 		if len(request_list):
 			api_request = request_list[0]
 			if api_request.results is not None:
 				latest_datetime = datetime.utcfromtimestamp(api_request.results[-1].created_utc)
+
+				for api_object in api_request.results:
+					if "remindme" in api_object.body.lower():
+						log.info(f"https://www.reddit.com{api_object.permalink}")
+
 				del request_list[0]
 
 		if queue.qsize() < 5:
@@ -91,7 +96,7 @@ async def producer(queue, start_id):
 					queued_id = request_list[-1].ids[-1][3:]
 
 				ids, last_id_str = get_next_hundred_ids(next_string_id(queued_id))
-				log.info(f"Queueing from {ids[0][3:]} to {ids[-1][3:]}")
+				log.debug(f"Queueing from {ids[0][3:]} to {ids[-1][3:]}")
 				api_request = ApiRequest(ids)
 				request_list.append(api_request)
 				queue.put_nowait(api_request)
@@ -126,30 +131,7 @@ async def main():
 	# 	task.cancel()
 	# await asyncio.gather(*workers, return_exceptions=True)
 
-
-
-
-	# reddit = asyncpraw.Reddit("Watchful1BotTest")
-	#
-	# start_id_str = "gmiz119"
-	#
-	# while True:
-	# 	ids, last_id_str = get_next_hundred_ids(start_id_str)
-	# 	first_date = None
-	# 	last_date = None
-	# 	count_found = 0
-	#
-	# 	submissions = await reddit.get(endpoints.API_PATH["info"], params={"id": ",".join(ids)})
-	# 	for submission in submissions:
-	# 		count_found += 1
-	# 		submission_date = datetime.utcfromtimestamp(submission.created_utc)
-	# 		if first_date is None:
-	# 			first_date = submission_date
-	# 		last_date = submission_date
-	# 	first_date_str = first_date.strftime('%Y-%m-%d %H:%M:%S') if first_date is not None else "None"
-	# 	last_date_str = last_date.strftime('%Y-%m-%d %H:%M:%S') if last_date is not None else "None"
-	# 	log.info(f"{start_id_str} to {last_id_str} : {count_found} : {first_date_str} - {last_date_str}")
-	# 	start_id_str = base36encode(base36decode(last_id_str) + 1)
+	# first_date.strftime('%Y-%m-%d %H:%M:%S')
 
 
 if __name__ == "__main__":
